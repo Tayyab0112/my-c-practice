807c75c8261b0301560e5ba8ad9079660649a3b0
Tayyab Khan <134232469+Tayyab0112@users.noreply.github.com>
2023-10-24 14:01:46 +0530

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    // array is collection of similar items
+    // it is like a variable that is capable of storing multiple items
+    int marks[10] = {1,2,3,4,45,6,7,8,9};
+     for (int i = 0; i<9; i++)
+     {
+        printf("%d \n",marks[i]);
+     }
+     
+return 0;
+}
